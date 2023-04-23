acf792e8dd2add3f70bee7f276543f50a15cba46
henrique24k <henriquevieira431@gmail.com>
2023-04-23 11:01:36 -0300

    add collection

---
+++ collections/memoriaArray.php
@@ -0,0 +1,11 @@
+<?php
+
+// $array = []; // memória para 0 itens
+// $array[] = 1; // memória para 2^1 = 2 itens
+// $array[] = 2; // memória para 2^1 = 2 itens
+// $array[] = 3; // memória para 2^2 = 4 itens
+// $array[] = 4; // memória para 2^2 = 4 itens
+// $array[] = 5; // memória para 2^3 = 8 itens
+
+
+
